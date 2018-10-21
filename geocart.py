2c98d756b8fbe5f1664291152ed94ca3bf7216f6
trungams <vttrung12@gmail.com>
2018-10-20 20:38:20 -0400

    Create Geo and Cartesian conversion util

---
+++ geocart.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''
+This module provides conversion between Geographic and Cartesian coordinates
+'''
+
+from math import cos, sin, asin, atan2, degrees, radians
+
+R = 6371.137        # km, constant in wgs84 datum
+
+def geo2cart(latitude, longitude):
+    latitude = radians(latitude)
+    longitude = radians(longitude)
+    x = R * cos(latitude) * cos(longitude)
+    y = R * cos(latitude) * sin(longitude)
+    z = R * sin(latitude)
+
+    return (x, y, z)
+
+def cart2geo(x, y, z):
+    latitude = degrees(asin(z / R))
+    longitude = degrees(atan2(y, x))
+
+    return (latitude, longitude)
